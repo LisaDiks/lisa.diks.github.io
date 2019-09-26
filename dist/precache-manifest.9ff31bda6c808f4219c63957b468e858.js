2570989da3326ecc8129d389815af67b86ab9a1c
Lisa Diks <lisa.diks@foursites.nl>
2019-09-26 16:17:20 +0200

    add dist map

---
+++ dist/precache-manifest.9ff31bda6c808f4219c63957b468e858.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "df18c4ad0070097f3e9f4826a412b968",
+    "url": "assets/fonts/Exo-Regular.woff"
+  },
+  {
+    "revision": "8f21e4b4064ab577116e",
+    "url": "main.js"
+  },
+  {
+    "revision": "2e81afa047e65f6700d7",
+    "url": "style.js"
+  },
+  {
+    "revision": "13c18fda7bf2d2d9",
+    "url": "spritemap.js"
+  },
+  {
+    "revision": "13c18fda7bf2d2d9",
+    "url": "spritemap.svg"
+  },
+  {
+    "revision": "2e81afa047e65f6700d7",
+    "url": "style.css"
+  },
+  {
+    "revision": "21bc2c05284c107746bd2cfc200495ca",
+    "url": "assets/fonts/FrederickatheGreat-Regular.woff2"
+  },
+  {
+    "revision": "84581915e870cee641c912a92f936dc5",
+    "url": "assets/fonts/FrederickatheGreat-Regular.woff"
+  },
+  {
+    "revision": "4ed1ec4588bfba8b63572219d608b1bf",
+    "url": "assets/fonts/Exo-Regular.woff2"
+  },
+  {
+    "revision": "36f611bdcaff7b003956033d2257a20f",
+    "url": "assets/fonts/Exo-Medium.woff2"
+  },
+  {
+    "revision": "7c797f574537e0d92ce5be3a4d55ed99",
+    "url": "assets/fonts/Exo-Medium.woff"
+  },
+  {
+    "revision": "920098eee0883350f721f420a291a2fe",
+    "url": "assets/fonts/Exo-Light.woff2"
+  },
+  {
+    "revision": "f343e6607642af8f2ceb8078c2b7fc63",
+    "url": "assets/fonts/Exo-Light.woff"
+  },
+  {
+    "revision": "d567fd85854b5fe54e4ffe42e75f567b",
+    "url": "assets/fonts/Exo-Bold.woff2"
+  },
+  {
+    "revision": "6cbb9452f7457bc1e693e3f3305c256c",
+    "url": "assets/fonts/Exo-Bold.woff"
+  }
+];
